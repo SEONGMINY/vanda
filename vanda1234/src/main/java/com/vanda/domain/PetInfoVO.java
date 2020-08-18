191104dcb2072570aabeb2a2020105e08ef5e8ed
user <user@bon400_12>
2020-08-18 14:34:42 +0900

    petINFO 명 바꿈

--- vanda1234/src/main/java/com/vanda/domain/PetInfoVO.java
+++ vanda1234/src/main/java/com/vanda/domain/PetInfoVO.java
@@ -2,61 +2,87 @@ package com.vanda.domain;
 
 public class PetInfoVO {
 
-   private String pet_name;
-   private double eat_gram;
-   private double pet_weight;
-   private double total_distance; 
-   private String timer;
+	private String pet_name;
+	private int pet_num;
+	private double pet_weight;
+	private double eat_gram;
+	private double gram;
+	private double avg; // 종 평균 몸무게
+	private String kind_name;
+	private double total_distance;
 
-   public PetInfoVO() {
-   }
+	public PetInfoVO() {
+	}
 
-   public PetInfoVO(String pet_name,double eat_gram, double pet_weight, double total_distance, String timer) {
-      this.pet_name=pet_name;
-      this.eat_gram = eat_gram;
-      this.pet_weight = pet_weight;
-      this.total_distance = total_distance;
-      this.timer = timer;
-   }
+	public PetInfoVO(String pet_name, double eat_gram, double pet_weight, double total_distance) {
+		this.pet_name = pet_name;
+		this.eat_gram = eat_gram;
+		this.pet_weight = pet_weight;
+		this.total_distance = total_distance;
+	}
 
-   public double getTotal_distance() {
-      return total_distance;
-   }
+	public double getTotal_distance() {
+		return total_distance;
+	}
 
-   public void setTotal_distance(double total_distance) {
-      this.total_distance = total_distance;
-   }
+	public void setTotal_distance(double total_distance) {
+		this.total_distance = total_distance;
+	}
 
-   public String getTimer() {
-      return timer;
-   }
+	public double getEat_gram() {
+		return eat_gram;
+	}
 
-   public void setTimer(String timer) {
-      this.timer = timer;
-   }
+	public void setEat_gram(double eat_gram) {
+		this.eat_gram = eat_gram;
+	}
 
-   public double getEat_gram() {
-      return eat_gram;
-   }
+	public double getPet_weight() {
+		return pet_weight;
+	}
 
-   public void setEat_gram(double eat_gram) {
-      this.eat_gram = eat_gram;
-   }
+	public void setPet_weight(double pet_weight) {
+		this.pet_weight = pet_weight;
+	}
 
-   public double getPet_weight() {
-      return pet_weight;
-   }
+	public String getPet_name() {
+		return pet_name;
+	}
 
-   public void setPet_weight(double pet_weight) {
-      this.pet_weight = pet_weight;
-   }
+	public void setPet_name(String pet_name) {
+		this.pet_name = pet_name;
+	}
 
-   public String getPet_name() {
-      return pet_name;
-   }
+	public int getPet_num() {
+		return pet_num;
+	}
 
-   public void setPet_name(String pet_name) {
-      this.pet_name = pet_name;
-   }
+	public void setPet_num(int pet_num) {
+		this.pet_num = pet_num;
+	}
+
+	public double getGram() {
+		return gram;
+	}
+
+	public void setGram(double gram) {
+		this.gram = gram;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public String getKind_name() {
+		return kind_name;
+	}
+
+	public void setKind_name(String kind_name) {
+		this.kind_name = kind_name;
+	}
 
 }
