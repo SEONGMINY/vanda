437f546aa3191958133dd2019ede135676f00bf1
alstlr0404 <64956006+alstlr0404@users.noreply.github.com>
2020-09-01 15:35:55 +0900

    petinfoVO

--- vanda1234/src/main/java/com/vanda/domain/PetInfoVO.java
+++ vanda1234/src/main/java/com/vanda/domain/PetInfoVO.java
@@ -2,23 +2,37 @@ package com.vanda.domain;
 
 public class PetInfoVO {
 
-	private String pet_name;
-	private int pet_num;
-	private double pet_weight;
-	private double eat_gram;
-	private double gram;
-	private double avg; // 종 평균 몸무게
-	private String kind_name;
-	private double total_distance;
-
+	private String pet_name;	//펫 이름
+	private int pet_num;	//펫 번호
+	private double pet_weight;		//펫 현재몸무게
+	private double eat_gram;		//펫 현재 먹은 배식량 
+	private double gram;			//펫 추천 배식량
+	private double avg; // 펫 권장 체중
+	private String kind_name;	
+	private double total_distance;	//현재 활동량
+	private double recomand_distance;	//추천 활동량
+	private int food_num;
+	
+	
 	public PetInfoVO() {
 	}
 
-	public PetInfoVO(String pet_name, double eat_gram, double pet_weight, double total_distance) {
+	public PetInfoVO(String pet_name, double eat_gram, double pet_weight, double total_distance, double recomand_distance,int food_num) {
 		this.pet_name = pet_name;
 		this.eat_gram = eat_gram;
 		this.pet_weight = pet_weight;
 		this.total_distance = total_distance;
+		this.recomand_distance = recomand_distance;
+		this.food_num = food_num;
+	}
+
+
+	public int getFood_num() {
+		return food_num;
+	}
+
+	public void setFood_num(int food_num) {
+		this.food_num = food_num;
 	}
 
 	public double getTotal_distance() {
@@ -84,5 +98,15 @@ public class PetInfoVO {
 	public void setKind_name(String kind_name) {
 		this.kind_name = kind_name;
 	}
+
+	public double getRecomand_distance() {
+		return recomand_distance;
+	}
+
+	public void setRecomand_distance(double recomand_distance) {
+		this.recomand_distance = recomand_distance;
+	}
+
+	
 
 }
