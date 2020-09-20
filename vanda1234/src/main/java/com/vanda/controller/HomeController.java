53729becf24670be48d9011c98de518afd105986
alstlr0404 <64956006+alstlr0404@users.noreply.github.com>
2020-09-20 10:51:11 +0900

    pet사료관련 home컨트롤러 업로드

--- vanda1234/src/main/java/com/vanda/controller/HomeController.java
+++ vanda1234/src/main/java/com/vanda/controller/HomeController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.vanda.domain.HospitalVO;
 import com.vanda.domain.UserVO;
+import com.vanda.service.ChatService;
+import com.vanda.service.FoodService;
+import com.vanda.service.KindService;
+import com.vanda.service.PetService;
 import com.vanda.service.UserService;
 
 /**
@@ -25,6 +29,16 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	UserService service;
+	@Autowired
+	private KindService kindService;
+	
+	@Autowired
+	private FoodService	foodService;
+	
+   @Autowired
+   private UserService userService;
+   @Autowired
+   private PetService petService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	*/
@@ -58,7 +72,17 @@ public class HomeController {
 		//로그인한유저가 등록된 병원 있으면 1 없으면 0
 		
 		UserVO loginUser = (UserVO) session.getAttribute("check");
-		
+		 System.out.println("펫등록 화면");
+			if (loginUser == null) {
+				System.out.println("로그인된 유저 없음 로그인화면 이동");
+
+				return "redirect:/user/login";
+			} else {
+				//견종 세션 및 견종에 해당하는 사료종류세션
+				session.setAttribute("kindList", kindService.kindList()); 
+				session.setAttribute("defaultFood", kindService.foodList(1));
+				System.out.println("펫종류 및 사료등록화면"+kindService.kindList());
+				
 		
 		if(loginUser.getUser_rule().equals("doctor")) {
 			
@@ -75,11 +99,12 @@ public class HomeController {
 				
 			}
 			
-		return "/myPage/myPage";
+			return "/myPage/myPage";
 			
-		}
-		return "/myPage/myPage";
+			}
+			return "/myPage/myPage";
 		
+			}
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
